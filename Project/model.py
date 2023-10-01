0a0dd9622728e9a2531d9cc224dc30223426a2f8
chenbingcheng <bingcheng.chen@outlook.com>
2023-10-01 21:23:45 +0200

    multihead self-attention

---
+++ Project/model.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import math
+
+
+class MultiHeadAttention(nn.Module):
+    """
+    Multihead self-attention
+    """
+
+    def __init__(self, num_heads, embedding_size):
+        """_summary_
+        """
+        super(MultiHeadAttention, self).__init__()
+        assert embedding_size % num_heads == 0, "embedding_size must be divisible by num_heads"
+
+        # initialize values
+        self.num_heads = num_heads
+        self.embedding_size = embedding_size
+        # Dimension of each head's key, query, and value
+        self.d_k = embedding_size // num_heads
+
+        # Linear layers for input embeddings, make sure Y = W_Q*X, Y has the same size with X, which means the size of W_Q, W_K, W_V is (embedding_size, embedding_size), the size of Q,K,V is (embedding_size, sequence_length)
+        self.W_q = nn.Linear(embedding_size, embedding_size)  # Query
+        self.W_k = nn.Linear(embedding_size, embedding_size)  # Key
+        self.W_v = nn.Linear(embedding_size, embedding_size)  # Value
+        self.W_o = nn.Linear(embedding_size, embedding_size)  # post attention
+
+    def normalized_dot_product_attention(self, Q, K, V, mask=None):
+        """_summary_
+
+        Args:
+            Q (torch tensor): Shape = (B, H, N, C)
+            K (torch tensor): Shape = (B, H, N, C)
+            V (torch tensor): Shape = (B, H, N, C)
+            mask (torch tensor, optional): mask for decoder multi head attention layer. Defaults to None.
+            Where,
+            B - Batch size
+            H - Number of heads
+            N - Sequence length
+            C - Embedding size
+        """
+        # Calculate attention scores
+        attn_scores = torch.matmul(
+            Q, K.transpose(-2, -1)) / math.sqrt(self.d_k)
+
+        # applying mask on attention, we use large negative values -1e9 instead of 0, because after softmax function, the result will be close to 0.
+        if mask is not None:
+            attn_scores = attn_scores.masked_fill(mask == 0, -1e9)
+
+        # Softmax is applied to obtain attention probabilities
+        attn_probs = torch.softmax(attn_scores, dim=-1)
+
+        # Multiply by values to obtain the final output
+        attn_output = torch.matmul(attn_probs, V)
+        return attn_output
+
+    def split_heads(self, x):
+        """ reshapes the input tensor x to have multiple heads for multi-head attention. 
+
+        Args:
+            x (_type_): _description_
+        """
+        batch_size, seq_length, embedding_size = x.size()
+        return x.view(batch_size, seq_length, self.num_heads, self.d_k).transpose(1, 2)
+
+    def combine_heads(self, x):
+        # Combine the multiple heads back to original shape
+        batch_size, _, seq_length, d_k = x.size()
+        return x.transpose(1, 2).contiguous().view(batch_size, seq_length, self.embedding_size)
+
+    def forward(self, Q, K, V, mask=None):
+        # Apply linear transformations and split heads
+        Q = self.split_heads(self.W_q(Q))
+        K = self.split_heads(self.W_k(K))
+        V = self.split_heads(self.W_v(V))
+
+        # Perform scaled dot-product attention
+        attn_output = self.normalized_dot_product_attention(Q, K, V, mask)
+
+        # Combine heads and apply output transformation
+        output = self.W_o(self.combine_heads(attn_output))
+        return output
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, embedding_size, max_seq_length):
+
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_seq_length, embedding_size)
+        position = torch.arange(
+            0, max_seq_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(
+            0, embedding_size, 2).float() * -(math.log(10000.0) / embedding_size))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        self.register_buffer('pe', pe.unsqueeze(0))
+
+    def forward(self, x):
+        """forward pass to generate positional embeddings
+
+        Args:
+            x - (torch tensor) embedded data. Shape = (B, N, C) 
+
+        Returns:
+            x - (torch tensor) positional embedded data. Shape = (B, N, C) 
+        """
+        return x + self.pe[:, :x.size(1)]
