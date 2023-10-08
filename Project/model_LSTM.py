4556b8f816a35c2356663940a9c953261f1948e8
chenbingcheng <bingcheng.chen@outlook.com>
2023-10-08 19:44:17 +0200

    update LSTMCell

--- Project/model_LSTM.py
+++ Project/model_LSTM.py
@@ -4,15 +4,21 @@ from torch.autograd import Variable
 import numpy as np
 
 
-class LSTMBlock(nn.Module):
+class LSTMCell(nn.Module):
+    """implement functionity for a LSTM cell 
+    """
 
     def __init__(self, input_size, hidden_size):
-        super(LSTMBlock, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
+        super(LSTMCell, self).__init__()
+        self.input_size = input_size  # The size of each input X_t
+        self.hidden_size = hidden_size  # The size of each hidden neuron
 
+        # W_hh * h_t_minus_1 + W_xh * x_t
+        # The reason we times 4 is that we have 4 gates
         self.W_hh = nn.Linear(hidden_size, hidden_size * 4)
         self.W_xh = nn.Linear(input_size, hidden_size * 4)
+
+        # reset the parameter
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -20,36 +26,43 @@ class LSTMBlock(nn.Module):
         for w in self.parameters():
             w.data.uniform_(-std, std)
 
-    def forward(self, input, hx=None):
+    def forward(self, x_t, h_t_c_t=None):
+        """compute hidden state h_t and cell state c_t, 
+        based on input x_t, and previous states h_{t-1} and c_{t-1}
 
-        # Inputs:
-        #       input: of shape (batch_size, input_size)
-        #       hx: of shape (batch_size, hidden_size)
-        # Outputs:
-        #       hy: of shape (batch_size, hidden_size)
-        #       cy: of shape (batch_size, hidden_size)
+        Args:
+            x_t (torch tensor): input at time t, shape (batch_size, input_size)
+            h_t_c_t (set of torch tensor): (h_{t-1}, c_{t-1}), each have the shape (batch_size, hidden_size)
 
-        if hx is None:
-            hx = Variable(input.new_zeros(input.size(0), self.hidden_size))
-            hx = (hx, hx)
+        Returns:
+            (h_t, c_t): each have the shape (batch_size, hidden_size)
+        """
 
-        hx, cx = hx
+        # Initiate hidden state h_t and cell state c_t for the first step
+        if h_t_c_t is None:
+            # zeros = Variable(x_t.new_zeros(x_t.size(0), self.hidden_size))
+            # h_t_c_t = (zeros, zeros)
+            zeros = torch.zeros(x_t.size(0), self.hidden_size, dtype=x_t.dtype)
+            h_t_c_t = (zeros, zeros)
 
-        gates = self.xh(input) + self.hh(hx)
+        h_t_1, c_t_1 = h_t_c_t
 
-        # Get gates (i_t, f_t, g_t, o_t)
-        input_gate, forget_gate, cell_gate, output_gate = gates.chunk(4, 1)
+        # gates = W_hh * h_t_1 + W_xh * x_t, gates has the shape (batch_size, 4*hidden_size)
+        gates = self.W_hh(h_t_1) + self.W_xh(x_t)
 
-        i_t = torch.sigmoid(input_gate)
-        f_t = torch.sigmoid(forget_gate)
-        g_t = torch.tanh(cell_gate)
-        o_t = torch.sigmoid(output_gate)
+        # Get 4 gates, each gate has the shape (batch_size, hidden_size)
+        input_gate, forget_gate, gate_gate, output_gate = gates.chunk(4, 1)
 
-        cy = cx * f_t + i_t * g_t
+        i = torch.sigmoid(input_gate)  # input gate
+        f = torch.sigmoid(forget_gate)  # forget gate
+        o = torch.sigmoid(output_gate)  # output gate
+        g = torch.tanh(gate_gate)  # gate gate
 
-        hy = o_t * torch.tanh(cy)
+        # according to LSTM architecture
+        c_t = f * c_t_1 + i * g # cell state 
+        h_t = o * torch.tanh(c_t) # hidden state
 
-        return (hy, cy)
+        return (h_t, c_t)
 
 
 class LSTM(nn.Module):
