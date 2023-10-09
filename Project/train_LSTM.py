e13bdedcd65a08f05956b838d402532beb1b3298
chenbingcheng <bingcheng.chen@outlook.com>
2023-10-09 11:41:22 +0200

    debug LSTM_train successfully for the first time

---
+++ Project/train_LSTM.py
@@ -0,0 +1,170 @@
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+import numpy as np
+import os
+import dataloader
+import model_LSTM
+import utils
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+# defining model save location
+save_location = "./models"
+# defining dataset locations
+dataset_folder = "./trajectory-prediction-transformers/datasets"
+dataset_name = "raw"
+# setting validation size. if val_size = 0, split percentage is 80-20
+val_size = 0
+# length of sequence given to encoder
+gt = 8
+# length of sequence given to decoder
+horizon = 12
+
+
+train_dataset, _ = dataloader.create_dataset(dataset_folder, dataset_name, val_size, \
+    gt, horizon, delim="\t", train=True)
+val_dataset, _ = dataloader.create_dataset(dataset_folder, dataset_name, val_size, \
+    gt, horizon, delim="\t", train=False)
+# test_dataset, _ = dataloader.create_dataset(dataset_folder, dataset_name, val_size, \
+#     gt, horizon, delim="\t", train=False, eval=True)
+
+
+# defining batch size
+batch_size = 64
+
+# creating torch dataloaders
+train_loader = DataLoader(train_dataset, batch_size, shuffle=True, num_workers=0)
+val_loader = DataLoader(val_dataset, batch_size, shuffle=True, num_workers=0)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+LSTM_model = model_LSTM.LSTM(input_size=2, input_seq_len=8, hidden_size=128, num_layers=2, output_size=2, output_seq_len=12)
+
+# number of epochs
+epochs = 1
+
+# metric variables
+training_loss = []
+validation_loss = []
+val_mad = []
+val_fad = []
+
+# Define the MSE loss function
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(LSTM_model.parameters(), lr=0.001)
+
+for epoch in tqdm(range(epochs)):
+    # TRAINING MODE
+    LSTM_model.train()
+
+    # training batch variables
+    train_batch_loss = 0
+
+    for idx, data in enumerate(train_loader):
+
+
+        # getting encoder input data
+        # (64, 7, 512)
+        X = data['src'][:, :, 0:2].to(device)
+
+        # if X.size(0) != 64:
+        #     break
+        # getting decoder input data
+        
+        target = data['trg'][:, :, 0:2].to(device)
+
+        # forward pass
+        optimizer.zero_grad()
+        # predictions = tf_model.forward(enc_input, dec_input, dec_source_mask, dec_target_mask)
+        predictions = LSTM_model.forward(X)
+
+        # calculating loss using pairwise distance of all predictions
+        # a = predictions.view(batch_size,-1)
+        # b = target.contiguous().view(batch_size,-1)
+
+        loss = criterion(predictions.view(X.size(0),-1), target.contiguous().view(X.size(0),-1))
+        train_batch_loss += loss.item()
+
+        # updating weights
+        loss.backward()
+        optimizer.step()
+
+    training_loss.append(train_batch_loss/len(train_loader))
+    print("Epoch {}/{}....Training loss = {:.4f}".format(epoch +
+            1, epochs, training_loss[-1]))
+
+    # # validation loop
+    # if (epoch+1) % 5 == 0:
+    #     with torch.no_grad():
+    #         # EVALUATION MODE
+    #         LSTM_model.eval()
+
+    #         # validation variables
+    #         batch_val_loss = 0
+    #         gt = []
+    #         pr = []
+
+    #         for id_b, data in enumerate(val_loader):
+    #             # storing groung truth
+    #             gt.append(data['trg'][:, :, 0:2])
+
+    #             # input to encoder input
+    #             val_input = (data['src'][:, 1:, 2:4].to(
+    #                 device)-mean.to(device))/std.to(device)
+
+    #             # input to decoder
+    #             start_of_seq = torch.Tensor([0, 0, 1]).unsqueeze(0).unsqueeze(
+    #                 1).repeat(val_input.shape[0], 1, 1).to(device)
+    #             dec_inp = start_of_seq
+    #             # decoder masks
+    #             dec_source_mask = torch.ones(
+    #                 (val_input.shape[0], 1, val_input.shape[1])).to(device)
+    #             dec_target_mask = utils.subsequent_mask(
+    #                 dec_inp.shape[1]).repeat(dec_inp.shape[0], 1, 1).to(device)
+
+    #             # prediction till horizon lenght
+    #             for i in range(horizon):
+    #                 # getting model prediction
+    #                 # model_output = tf_model.forward(val_input, dec_inp, dec_source_mask, dec_target_mask)
+    #                 model_output = tf_model.forward(val_input, dec_inp)
+
+    #                 # appending the predicition to decoder input for next cycle
+    #                 dec_inp = torch.cat(
+    #                     (dec_inp, model_output[:, -1:, :]), 1)
+
+    #             # calculating loss using pairwise distance of all predictions
+    #             val_loss = F.pairwise_distance(dec_inp[:, 1:, 0:2].contiguous().view(-1, 2),
+    #                                             ((data['trg'][:, :, 2:4].to(device)-mean.to(device))/std.to(device)).
+    #                                             contiguous().view(-1, 2).to(device)).mean() + \
+    #                 torch.mean(torch.abs(dec_inp[:, 1:, 2]))
+    #             batch_val_loss += val_loss.item()
+
+    #             # calculating the position for each time step of prediction based on velocity
+    #             preds_tr_b = (dec_inp[:, 1:, 0:2]*std.to(device) + mean.to(device)).cpu().numpy().cumsum(1) + \
+    #                 data['src'][:, -1:, 0:2].cpu().numpy()
+
+    #             pr.append(preds_tr_b)
+    #         validation_loss.append(batch_val_loss/len(val_loader))
+
+    #         # calculating mad and fad evaluation metrics
+    #         gt = np.concatenate(gt, 0)
+    #         pr = np.concatenate(pr, 0)
+    #         mad, fad, _ = dataloader.distance_metrics(gt, pr)
+    #         val_mad.append(mad)
+    #         val_fad.append(fad)
+
+    #         print("Epoch {}/{}....Validation mad = {:.4f}, Validation fad = {:.4f}".format(
+    #             epoch+1, epochs, mad, fad))
+
+    # # Saving model, loss and error log files
+    # torch.save({
+    #     'model_state_dict': tf_model.state_dict(),
+    #     'optimizer_state_dict': optimizer.state_dict(),
+    #     'training_loss': training_loss,
+    #     'validation_loss': validation_loss,
+    #     'val_mad': val_mad,
+    #     'val_fad': val_fad,
+    #     'learning_rate': learning_rate
+    # }, os.path.join(save_location, 'epoch{}.pth'.format(epoch+1)))
